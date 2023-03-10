9f864901d614051056bc3c3cb310bb3f1bf2c541
nicosis <nicosis@gmail.com>
2023-03-10 20:33:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
-import "bootstrap";
+/* import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() { */
+// console.log("Hello Rigo from the console!");
+//write your code here
+
+function combination() {
+  let pronoun = ["el", "la"];
+  let adj = ["gran", "chocho"];
+  let noun = ["jogger", "racoon"];
+  let extention = ["com", "net", "org", "es"];
+
+  let string = [];
+
+  for (let i of pronoun) {
+    for (let x of adj) {
+      for (let y of noun) {
+        for (let z of extention) {
+          string.push(i + x + y + "." + z);
+        }
+      }
+    }
+  }
+
+  return string;
+}
+
+console.log(combination());
